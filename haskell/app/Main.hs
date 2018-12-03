cc19506a3f2b2ab49d00b02fffd416ed7969888c
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-12-03 09:32:46 +0100

    Solves day3 pt1

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+import qualified Day03
+
+putStrLnExtra :: String -> IO ()
+putStrLnExtra s = putStrLn $ s ++ "\n"
+
+solve :: (Show a) => (String -> a) -> FilePath -> String -> IO ()
+solve f path info = do
+  contents <- readFile path
+  putStrLnExtra $ info ++ ": " ++ show (f contents)
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLnExtra "-- Solutions --"
+  solve Day03.partOne "puzzleInputs/day03.txt" "Day 03 part 1"
+  -- map solve [solve Day03.partOne "puzzleInputs/day03.txt" "Day 03 part 1"]
+  putStrLnExtra "-- End --"
--- haskell/src/Day03.hs
+++ haskell/src/Day03.hs
@@ -1,23 +1,58 @@
 module Day03 where
 
+import Data.List.Split
+import qualified Data.Map.Strict as Map
 import Data.Maybe
 
+type Dimensions = (Int, Int)
+
+type Position = (Int, Int)
+
+type ClaimMap = Map.Map Int (Map.Map Int Int)
+
 data Claim = Claim
-  { id :: Int
-  , position :: (Int, Int)
-  , dimensions :: (Int, Int)
-  } deriving (Show)
+  { claimId :: Int
+  , position :: Position
+  , dimensions :: Dimensions
+  } deriving (Show, Eq)
+
+readId :: String -> Maybe Int
+readId ('#':num) = Just (read num)
+readId _ = Nothing
+
+toVector :: String -> Maybe (Int, Int)
+toVector = readComponents . endByOneOf ",:x"
+  where
+    readComponents [x, y] = Just (read x, read y)
+    readComponents _ = Nothing
 
 parseClaim :: String -> Maybe Claim
 parseClaim = parse . words
   where
-    parse [claimId, at, pos, dim] =
-      Just $ Claim {Day03.id = 1, position = (2, 2), dimensions = (2, 2)}
+    parse [cId, at, pos, dim] =
+      case (readId cId, sequence [toVector pos, toVector dim]) of
+        (Just i, Just [p, d]) ->
+          Just $ Claim {claimId = i, position = p, dimensions = d}
+        _ -> Nothing
     parse _ = Nothing
 
--- parseClaim = const Claim {Day03.id = 1, position = (2, 2), dimensions = (2, 2)}
-parseInput :: String -> [Maybe Claim]
-parseInput = filter isJust . map parseClaim . lines
+parseInput :: String -> [Claim]
+parseInput = mapMaybe parseClaim . lines
+
+addSquare :: Position -> ClaimMap -> ClaimMap
+addSquare (x, y) claims =
+  let xs = Map.findWithDefault Map.empty y claims
+   in Map.insert y (Map.insertWith (+) x 1 xs) claims
+
+addClaim :: Claim -> ClaimMap -> ClaimMap
+addClaim claim claims =
+  let (x, y) = position claim
+      (width, height) = dimensions claim
+      squares = [(x, y) | x <- [x .. x + width - 1], y <- [y .. y + height - 1]]
+   in foldr addSquare claims squares
 
 partOne :: String -> Int
-partOne = length
+partOne = countOverlaps . toMap . parseInput
+  where
+    toMap = foldr addClaim Map.empty
+    countOverlaps = sum . map (length . filter ((<) 1) . Map.elems) . Map.elems
