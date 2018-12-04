7673dc8d9806cc461e252f8e242897ee32105bef
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-12-04 09:05:53 +0100

    Refactors and cleans up day 3. Tuples as keys rather than 2D maps, sets rather than arrays

--- haskell/src/Day03.hs
+++ haskell/src/Day03.hs
@@ -11,7 +11,7 @@ type Position = (Int, Int)
 
 type ClaimId = Int
 
-type ClaimMap = Map.Map Int (Map.Map Int [Int])
+type ClaimMap = Map.Map Position (Set.Set ClaimId)
 
 createClaimMap :: [Claim] -> ClaimMap
 createClaimMap = foldr addClaim Map.empty
@@ -35,7 +35,7 @@ toVector = readComponents . endByOneOf ",:x"
 parseClaim :: String -> Maybe Claim
 parseClaim = parse . words
   where
-    parse [cId, at, pos, dim] =
+    parse [cId, _, pos, dim] =
       case (readId cId, sequence [toVector pos, toVector dim]) of
         (Just i, Just [p, d]) ->
           Just $ Claim {claimId = i, position = p, dimensions = d}
@@ -46,31 +46,31 @@ parseInput :: String -> [Claim]
 parseInput = mapMaybe parseClaim . lines
 
 claimSquare :: ClaimId -> Position -> ClaimMap -> ClaimMap
-claimSquare cId (x, y) claims =
-  let xs = Map.findWithDefault Map.empty y claims
-   in Map.insert y (Map.insertWith (++) x [cId] xs) claims
+claimSquare cId pos claims =
+  Map.insertWith Set.union pos (Set.singleton cId) claims
 
 addClaim :: Claim -> ClaimMap -> ClaimMap
 addClaim claim claims =
-  let (x, y) = position claim
+  let (left, top) = position claim
       cId = claimId claim
       (width, height) = dimensions claim
-      squares = [(x, y) | x <- [x .. x + width - 1], y <- [y .. y + height - 1]]
+      squares =
+        [ (x, y)
+        | x <- [left .. left + width - 1]
+        , y <- [top .. top + height - 1]
+        ]
    in foldr (claimSquare cId) claims squares
 
 partOne :: String -> Int
 partOne = countOverlaps . createClaimMap . parseInput
   where
-    countOverlaps =
-      sum . map (length . filter ((<) 1 . length) . Map.elems) . Map.elems
+    countOverlaps = length . filter ((<) 1 . length) . Map.elems
 
 partTwo :: String -> Maybe ClaimId
 partTwo input =
   let claims = parseInput input
       claimMap = createClaimMap claims
       overlappingClaims =
-        Set.fromList .
-        concat . map (concat . filter ((<) 1 . length) . Map.elems) $
-        Map.elems claimMap
+        Set.unions . filter ((<) 1 . length) $ Map.elems claimMap
       ids = Set.fromList $ map claimId claims
    in Set.lookupMin $ Set.difference ids overlappingClaims
